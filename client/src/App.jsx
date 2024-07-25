5f482fed04eea7d27c9a6c8323017622275af263
ShadyKahouech <shady.kahouech@gmail.com>
2024-07-25 15:33:30 +0100

    second commit

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,18 +5,22 @@ import "./App.css";
 import BackOffice from "./components/BackOffice.jsx";
 import Login from "./components/Login.jsx";
 import Cart from "./components/Cart.jsx";
+import Footer from "./components/Footer.jsx";
 
 function App() {
   return (
     <div className="app">
       <NavBar />
+      <div>
+        <Routes>
+          <Route path="/" element={<Home />} />
 
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/backOffice" element={<BackOffice />} />
-        <Route path="/cart" element={<Cart />} />
-      </Routes>
+          <Route path="/login" element={<Login />} />
+          <Route path="/backOffice" element={<BackOffice />} />
+          <Route path="/cart" element={<Cart />} />
+        </Routes>
+      </div>
+        <Footer />
     </div>
   );
 }
