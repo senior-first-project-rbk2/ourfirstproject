248e606ffe14d568732f49a3760539465c319b69
ShadyKahouech <shady.kahouech@gmail.com>
2024-07-28 16:56:26 +0100

    7th commit

--- client/src/components/Cart.jsx
+++ client/src/components/Cart.jsx
@@ -1,98 +1,148 @@
-import React from "react"
-import axios from 'axios'
-import { useState, useEffect } from "react"
-import {jwtDecode}from "jwt-decode"
-import { useNavigate } from "react-router-dom"
+import React from "react";
+import axios from "axios";
+import { useState, useEffect } from "react";
+import { jwtDecode } from "jwt-decode";
+import { useNavigate } from "react-router-dom";
 
-function Cart({setTotal}) {
-  const [cartitems, setCartitems]=useState([])
-  const [refresh, setRefresh]=useState(false)
+function Cart({ setTotal }) {
+  const [cartitems, setCartitems] = useState([]);
+  const [refresh, setRefresh] = useState(false);
   const navigate = useNavigate();
-  
 
-  const getCartByUser= ()=>{
+  const getCartByUser = () => {
+    const token = localStorage.getItem("user");
 
-    
-
-    const token = localStorage.getItem("user")
-  
-    const decodedToken = jwtDecode(token)
-    console.log("decodedtoken", decodedToken)
-    const userID = decodedToken.userId
-    axios.get(`http://localhost:5000/cart/getcartbyuser/${userID}`).then((res)=>{
-      const itemsdata = res.data
-      const newdata= itemsdata.map((item)=>(
-        {
+    const decodedToken = jwtDecode(token);
+    console.log("decodedtoken", decodedToken);
+    const userID = decodedToken.userId;
+    axios
+      .get(`http://localhost:5000/cart/getcartbyuser/${userID}`)
+      .then((res) => {
+        const itemsdata = res.data;
+        const newdata = itemsdata.map((item) => ({
           id: item.id,
           quantity: item.quantity,
           name: item.product.name,
           productImage: item.product.imageUrl,
-          productprice: item.product.price
-          
-        }
-      ))
-      setCartitems(newdata)
-      console.log("cartitems", cartitems)
-    }).catch((error)=>{
-      console.log("error fetching the content of the cart",error)
-    })
-  }
-
-  const updatequantity=(itemid, newquantity)=>{
-    if (newquantity <= 0) return
-    axios.put(`http://localhost:5000/cart/update/${itemid}`, {quantity: newquantity}).then(()=>{
-      setRefresh(!refresh)
-      }) 
-    .catch((error)=>{
-      console.log("error updating the item", error)
-    })
-  }
-
-  const deleteProdfromCart= (id)=>{
-
-    axios.delete(`http://localhost:5000/cart/delete/${id}`).then(()=>{
-      setRefresh(!refresh)
-    }).catch((error)=>{
-      console.log("error deleting the product", error)
-    })
-  }
-
-  const calculateTotalSum = () => {
-    return cartitems.reduce((sum, item) => sum + item.quantity * item.productprice, 0);
+          productprice: item.product.price,
+        }));
+        setCartitems(newdata);
+        console.log("cartitems", cartitems);
+      })
+      .catch((error) => {
+        console.log("error fetching the content of the cart", error);
+      });
   };
 
-  const totalSum = calculateTotalSum()
-  console.log("total sum", totalSum)
+  const updatequantity = (itemid, newquantity) => {
+    if (newquantity <= 0) return;
+    axios
+      .put(`http://localhost:5000/cart/update/${itemid}`, {
+        quantity: newquantity,
+      })
+      .then(() => {
+        setRefresh(!refresh);
+      })
+      .catch((error) => {
+        console.log("error updating the item", error);
+      });
+  };
 
-  useEffect(()=>{getCartByUser()},[refresh])
+  const deleteProdfromCart = (id) => {
+    axios
+      .delete(`http://localhost:5000/cart/delete/${id}`)
+      .then(() => {
+        setRefresh(!refresh);
+      })
+      .catch((error) => {
+        console.log("error deleting the product", error);
+      });
+  };
 
-  return (
-    <div className="display-products-cart">
-      {cartitems.map((item)=>{
-        return <div className="one-item">
-          <div className="product-name">{item.name}</div>
-          <img className="product-image" src={item.productImage} alt={item.name} />
-          <div className="product-quantity">
-              <button onClick={()=>{updatequantity(item.id, item.quantity-1)}}>-</button>
-              <input 
-                type="text" 
-                value={item.quantity} 
-                readOnly
-              />
-              <button onClick={()=>{updatequantity(item.id, item.quantity+1)}}>+</button>
-            </div>
-          <button className="delete-product" onClick={()=>{deleteProdfromCart(item.id)}}>Delete</button>
-         
+  const calculateTotalSum = () => {
+    return cartitems.reduce(
+      (sum, item) => sum + item.quantity * item.productprice,
+      0
+    );
+  };
 
+  const totalSum = calculateTotalSum();
+  console.log("total sum", totalSum);
+
+  useEffect(() => {
+    getCartByUser();
+  }, [refresh]);
+
+  if (cartitems.length === 0) {
+    return (
+      <div className="icon__shopping__cart">
+        <img
+          className="shopping__cart_img"
+          src="./src/assets/image.png"
+          alt="Shopping cart"
+        />
+        <div className="icon__details">
+          <h3>Your shopping cart is empty</h3>
+          <span>
+            You have no items in your shopping cart, let's add some items you
+            like.
+          </span>
         </div>
-      })}
-      <div className="buy-bouton"> 
-        <button onClick={()=>{
-          navigate("/useradress"), setTotal(totalSum) }}>Buy</button>
       </div>
-      
-    </div>
-  );
+    );
+  } else {
+    return (
+      <div className="display-products-cart">
+        {cartitems.map((item) => {
+          return (
+            <div className="one-item">
+              <div className="product-name">{item.name}</div>
+              <img
+                className="product-image"
+                src={item.productImage}
+                alt={item.name}
+              />
+              <div className="product-quantity">
+                <button
+                  onClick={() => {
+                    updatequantity(item.id, item.quantity - 1);
+                  }}
+                >
+                  -
+                </button>
+                <input type="text" value={item.quantity} readOnly />
+                <button
+                  onClick={() => {
+                    updatequantity(item.id, item.quantity + 1);
+                  }}
+                >
+                  +
+                </button>
+              </div>
+              <button
+                className="delete-product"
+                onClick={() => {
+                  deleteProdfromCart(item.id);
+                }}
+              >
+                Delete
+              </button>
+            </div>
+          );
+        })}
+
+        <div className="buy-bouton">
+          <button
+            onClick={() => {
+              navigate("/useradress"), setTotal(totalSum);
+            }}
+          >
+            Buy
+          </button>
+        </div>
+      </div>
+    );
+  }
 }
 
-export default Cart
+export default Cart;
--- client/src/components/Register.jsx
+++ client/src/components/Register.jsx
@@ -1,6 +1,70 @@
+// import React, { useState } from "react";
+// import { addUsers } from "../services/addUsersService";
+// import { useNavigate } from "react-router-dom";
+
+// function Register() {
+//   const [username, setUserName] = useState("");
+//   const [email, setEmail] = useState("");
+//   const [password, setPassword] = useState("");
+//   const [confirmPassword, setConfirmPassword] = useState("");
+//   const navigate = useNavigate();
+
+//   const handleChangeName = (e) => {
+//     setUserName(e.target.value);
+//   };
+
+//   const handleChangeEmail = (e) => {
+//     setEmail(e.target.value);
+//   };
+
+//   const handleChangePassword = (e) => {
+//     setPassword(e.target.value);
+//   };
+//   const handleChangeConfirmPassword = (e) => {
+//     setConfirmPassword(e.target.value);
+//   };
+//   const handleClick = () => {
+//     if (password !== confirmPassword) {
+//       alert("Passwords do not match");
+//       return;
+//     } else {
+//       //check if email exist
+//       addUsers();
+//     }
+
+//     addUsers(username, email, password)
+//       .then((res) => {
+//         localStorage.setItem("user", res.data.token);
+//         navigate("/");
+//       })
+//       .catch((err) => {
+//         console.error(err);
+//         alert("Password is not matching");
+//       });
+//   };
+//   return (
+//     <div className="register__container">
+//       <h3>Register Now</h3>
+//       Your User Name:
+//       <input onChange={handleChangeName} />
+//       Your User Email:
+//       <input onChange={handleChangeEmail} />
+//       Your Password:
+//       <input onChange={handleChangePassword} />
+//       Confirm Your Password:
+//       <input onChange={handleChangeConfirmPassword} />
+//       <button className="login__btn" onClick={handleClick}>
+//         Confirm
+//       </button>
+//     </div>
+//   );
+// }
+
+// export default Register;
+
 import React, { useState } from "react";
-import { addUsers } from "../services/addUsersService";
 import { useNavigate } from "react-router-dom";
+import axios from "axios";
 
 function Register() {
   const [username, setUserName] = useState("");
@@ -28,19 +92,18 @@ function Register() {
       alert("Passwords do not match");
       return;
     } else {
-      //check if email exist 
-      addUsers();
+      //check if email exist
+      axios
+        .post("http://localhost:5000/users/add", { username, email, password })
+        .then((res) => {
+          localStorage.setItem("user", res.data.token);
+          navigate("/");
+        })
+        .catch((err) => {
+          console.error(err);
+          throw err;
+        });
     }
-
-    addUsers(username, email, password)
-      .then((res) => {
-        localStorage.setItem("user", res.data.token);
-        navigate("/");
-      })
-      .catch((err) => {
-        console.error(err);
-        alert("Password is not matching");
-      });
   };
   return (
     <div className="register__container">
